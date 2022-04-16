a74f984ae1819e62937c4906197091d6cf6c87f2
MDanielMacedo <mdanielmacedo78@gmail.com>
2022-04-16 14:54:45 -0300

    debug no arquivo 'personalizado.js'

--- js/personalizado.js
+++ js/personalizado.js
@@ -2,7 +2,7 @@ function pagamento(){
     console.log('Acessou');
     var endereco = jQuery('.endereco').attr('data-endereco');
     $.ajax({
-        url: endereco + "./pagamento.php",
+        url: endereco + "pagamento.php",
         type: 'POST',
         datatype: 'json',
         success: function(retorno){

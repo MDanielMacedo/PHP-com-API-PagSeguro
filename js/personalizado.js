f26046196c892255080fe990894b932542b5f9bb
MDanielMacedo <mdanielmacedo78@gmail.com>
2022-04-16 13:35:39 -0300

    testes deploy em tempo real

--- js/personalizado.js
+++ js/personalizado.js
@@ -8,6 +8,27 @@ function pagamento(){
         success: function(retorno){
             // console.log(retorno);
             PagSeguroDirectPayment.setSessionId(retorno.id);
+        },
+        complete: function(retorno) {
+            // Callback para todas chamadas.
+            listarMeiosPag();
         }
     });
+}
+
+function listarMeiosPag(){
+    PagSeguroDirectPayment.getPaymentMethods({
+        amount: 500.00,
+        success: function(retorno) {
+            // Retorna os meios de pagamento disponíveis.
+            console.log(retorno);
+        },
+        error: function(retorno) {
+            // Callback para chamadas que falharam.
+        },
+        complete: function(retorno) {
+            // Callback para todas chamadas.
+        }
+    });
+
 }
